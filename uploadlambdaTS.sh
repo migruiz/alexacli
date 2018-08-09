da8098731e654d8810f6824e3cfc951949ef1c34
Miguel Ruiz <miguel@dpsystems.ie>
2018-08-09 16:30:18 +0100

    TS

--- uploadlambdaTS.sh
+++ uploadlambdaTS.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd /code/lambda
-/usr/local/bin/tsc
 /usr/local/bin/npm install
+/usr/local/bin/tsc
 /usr/bin/zip -X -r   ../lambda.zip *
 /root/.local/bin/aws lambda update-function-code --function-name $1 --zip-file  fileb:///code/lambda.zip
-
+/root/.local/bin/aws lambda update-function-configuration --function-name $1 --handler dist/index.handler --runtime nodejs8.10
 
 
 
