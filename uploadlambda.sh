6563ca2c3681c5515032c6bc0aa824bb3bf012fe
Miguel Ruiz <miguel@dpsystems.ie>
2018-08-09 15:46:12 +0100

    rrr

--- uploadlambda.sh
+++ uploadlambda.sh
@@ -2,7 +2,7 @@
 cd /code/lambda
 /usr/local/bin/npm install
 /usr/bin/zip -X -r   ../lambda.zip *
-cd ..
+ls ~/.local/bin
 /root/.local/bin/aws lambda update-function-code --function-name $1 --zip-file  fileb:///code/lambda.zip
 
 
